d91c31780212fa4c0c546536ce79ce413929786e
christopher124 <63984433+christopher124@users.noreply.github.com>
2020-09-08 09:25:38 -0500

    primer commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./gql/schema");
+const resolvers = require("./gql/resolver");
+require("dotenv").config({ path: ".env" });
+
+mongoose.connect(
+  process.env.BBDD,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+    useCreateIndex: true,
+  },
+  (err, _) => {
+    if (err) {
+      console.log("error de conexion");
+    } else {
+      server();
+    }
+  }
+);
+
+function server() {
+  const serverApollo = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  serverApollo.listen().then(({ url }) => {
+    console.log("###################");
+    console.log(`Servidor listo en la url ${url}`);
+    console.log("###################");
+  });
+}
